e3dc2e74d3ccf3d442cb66c130122218b3997411
tomaskotuc <tomask1298@gmail.com>
2025-05-14 14:58:19 +0200

    HOTOVO KONEC CIGOS ZVONEC

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/pokorny/cykloweby';
+    public string $baseURL = 'http://localhost/kotuc/cykloweby/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
