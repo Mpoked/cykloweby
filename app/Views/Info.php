9d628d3b41cbef33256baa1482de024d83ec994c
tomaskotuc <tomask1298@gmail.com>
2025-05-21 14:06:10 +0200

    prosiiiiiiiiiiiiiiiiiiiiiiiim

--- app/Views/Info.php
+++ app/Views/Info.php
@@ -5,8 +5,8 @@
 <div class="container mt-4">
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
 
-       
-        <?php foreach ($zavody as $zavod): 
+        
+       <?php foreach ($zavody as $zavod): 
             $zacatek_zavodu = strtotime($zavod->start_date);
             $zacatek_zavodu = date("j.n.Y", $zacatek_zavodu);
 
--- app/Views/stages.php
+++ app/Views/stages.php
@@ -1,14 +1,13 @@
 <?= $this->extend('layout/template'); ?>
 <?= $this->section('content'); ?>
 
-
 <div class="container py-5">
-    <h1 class="mb-4">Etapy závodu</h1>
+    <h1 class="mb-4 text-center">Etapy závodu</h1>
 
     <?php if (!empty($stages)): ?>
         <div class="table-responsive">
             <table class="table table-striped table-hover align-middle">
-                <thead class="table-dark">
+                <thead class="table-dark text-center">
                     <tr>
                         <th>Etapa</th>
                         <th>Datum</th>
@@ -19,14 +18,14 @@
                         <th>Převýšení</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody class="text-center">
                     <?php foreach ($stages as $stage): ?>
                         <tr>
                             <td><?= esc($stage['number']) ?></td>
-                            <td><?= date('d. m. Y', strtotime($stage['date'])) ?></td>
+                            <td><?= date('j. n. Y', strtotime($stage['date'])) ?></td>
                             <td><?= esc($stage['departure']) ?></td>
                             <td><?= esc($stage['arrival']) ?></td>
-                            <td><?= esc($stage['distance']) ?> km</td>
+                            <td><?= number_format($stage['distance'], 1) ?> km</td>
                             <td><?= esc($stage['parcour_type_text']) ?></td>
                             <td><?= esc($stage['vertical_meters']) ?> m</td>
                         </tr>
@@ -35,11 +34,8 @@
             </table>
         </div>
     <?php else: ?>
-        <div class="alert alert-info">Pro tento závod zatím nejsou dostupné žádné etapy.</div>
+        <div class="alert alert-info text-center">Pro tento závod zatím nejsou dostupné žádné etapy.</div>
     <?php endif; ?>
 </div>
-
-</body>
-
 
 <?= $this->endSection(); ?>
